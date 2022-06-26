5e6dc6b0ab25f18190e333ecdbce82cbc16bcc00
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-26 04:20:15 +0100

    Accepts input and gives output, Usersname

---
+++ Javascript/helloworld.js
@@ -0,0 +1,8 @@
+console.log('Hello world');
+
+
+const prompt = require('prompt-sync')();
+let fname = prompt('Enter your first name: ');
+let lname = prompt('Enter your last name: ');
+
+console.log('Your names are ', fname, lname);
